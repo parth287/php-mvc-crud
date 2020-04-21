c5aa6b770d4ecd8ffc3d9fee28d0069d792575e4
Parth Doshi <pdoshi287@gmail.com>
2020-04-21 15:00:19 +0530

    v1.0

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once ("class/DBController.php");
+require_once ("class/Student.php");
+
+$db_handle = new DBController();
+
+$action = "";
+if (! empty($_GET["action"])) {
+    $action = $_GET["action"];
+}
+switch ($action) {
+    case "student-add":
+        if (isset($_POST['add'])) {
+            $name = $_POST['name'];
+            $roll_number = $_POST['roll_number'];
+            $dob = "";
+            if ($_POST["dob"]) {
+                $dob_timestamp = strtotime($_POST["dob"]);
+                $dob = date("Y-m-d", $dob_timestamp);
+            }
+            $class = $_POST['class'];
+            
+            $student = new Student();
+            $insertId = $student->addStudent($name, $roll_number, $dob, $class);
+            if (empty($insertId)) {
+                $response = array(
+                    "message" => "Problem in Adding New Record",
+                    "type" => "error"
+                );
+            } else {
+                header("Location: index.php");
+            }
+        }
+        require_once "web/student-add.php";
+        break;
+    
+    case "student-edit":
+        $student_id = $_GET["id"];
+        $student = new Student();
+        
+        if (isset($_POST['add'])) {
+            $name = $_POST['name'];
+            $roll_number = $_POST['roll_number'];
+            $dob = "";
+            if ($_POST["dob"]) {
+                $dob_timestamp = strtotime($_POST["dob"]);
+                $dob = date("Y-m-d", $dob_timestamp);
+            }
+            $class = $_POST['class'];
+            
+            $student->editStudent($name, $roll_number, $dob, $class, $student_id);
+            
+            header("Location: index.php");
+        }
+        
+        $result = $student->getStudentById($student_id);
+        require_once "web/student-edit.php";
+        break;
+    
+    case "student-delete":
+        $student_id = $_GET["id"];
+        $student = new Student();
+        
+        $student->deleteStudent($student_id);
+        
+        $result = $student->getAllStudent();
+        require_once "web/student.php";
+        break;
+    
+    default:
+        $student = new Student();
+        $result = $student->getAllStudent();
+        require_once "web/student.php";
+        break;
+}
+?>
